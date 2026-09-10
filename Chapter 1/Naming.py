402b9c27e1b198d2eb06b933ed7f37c5bb1fda6a
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-10 11:02:23 +0100

    Create Naming.py with string manipulation examples
    Add initial implementation of naming questions and string manipulations.

---
+++ Chapter 1/Naming.py
@@ -0,0 +1,30 @@
+#myName = "Frank Lampey"
+#print(myName[6:10])
+
+#print(myName[len(myName)-1])
+
+#myNAme = "George Clooney"
+#print(myName.__contains__(" "))
+
+#question 1
+
+#print("what is your favourite artist!")
+#fav = input("Enter your favourite artist")
+#string1 = fav 
+#string2 = " is brilliant!"
+#answer = string1+string2
+#print(answer)
+
+#question 2
+
+#fullName = input("What is your name?")
+#space = fullName.index(" ")
+#fName = fullName
+#sName = fullName
+#fName = (fullName[0:space])
+#sName = (fullName[space:1000000])
+#print(fName)
+#print(sName)
+
+#question 3
+
