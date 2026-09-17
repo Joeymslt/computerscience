bd8f0e969b60e65769a37c9f41f7b9a53fc78552
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-17 10:52:44 +0100

    Add password input and verification logic
    Implement password verification loop and welcome message.

---
+++ Chapter 1/Password.py
@@ -0,0 +1,13 @@
+#balance = 0
+#if balance <= 0:
+#    print ("SORRY YOU GOT NO MONEY")
+    
+#difference between if & while is if only does it once while while does until changed
+
+correctPassword = ("joemama")
+password = input("Enter your password!")
+while correctPassword != password:
+    print("Don't let them in")
+
+if password == ("joemama") :
+    print("Welcome!")
