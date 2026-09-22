782175bb9aa730b4ddb867dc12c28af062eb17c0
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-22 17:22:13 +0100

    Add initial cinema booking script in test.py

---
+++ Chapter 1/test.py
@@ -0,0 +1,29 @@
+cinemaName = ("Tralee Movie House")
+print (cinemaName)
+
+tPrice = 8.5
+bFee = 2.00
+print("Welcome to Tralee Movie House!")
+print("Booking fee:")
+print(bFee)
+name = input("Please enter your full name")
+nLength = len(name)
+print("The length of your name is:")
+print(nLength)
+age = (int(input("Please enter your age")))
+tNumber = (int(input("How many tickets would you like")))
+tTC = (tNumber*tPrice)
+print("The total ticket price will be")
+print(tTC)
+print("All together that will be:")
+total = (tNumber*tPrice+bFee)
+pPP = (total/tNumber)
+print(total)
+print("Per person will be:")
+fPrice = (tTC/tNumber)
+print(fPrice)
+nme = name[0:3]
+print("First three leters of your name is:")
+print(nme)
+
+
