4995d4341abbf5d195199634cd35c10b6517879f
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-24 11:02:42 +0100

    Add hw2.py to demonstrate string manipulation

---
+++ Chapter 1/hw2.py
@@ -0,0 +1,9 @@
+myString = "Frank Lampey"
+print(myString)
+myStringList = list(myString)
+print(myStringList)
+
+print(type(myString))
+print(type(myStringList))
+
+myStringList = ['F','r','a','n','k',' '
