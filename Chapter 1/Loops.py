90fd027f65b79984787004577200bf332dba92db
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-24 10:30:00 +0100

    Add commented-out loop examples in Loops.py

---
+++ Chapter 1/Loops.py
@@ -0,0 +1,59 @@
+#for y in range (1,5):
+#    print(y)
+    
+#for y in range(1,5):
+#    print(y)
+#for y in range(1,5):
+#    print(y)
+#print(y)
+
+#counter = 1
+#while(counter<5):
+#    print(counter)
+#    counter+=1
+#while(counter<5):
+#    print(counter)
+#    counter+=1
+#print(counter)
+
+#for hNum in range(50,71):
+#    print(hNum,"St. Patrick's Parade")
+
+#for y in range(5):
+#    print(y)
+    
+#for y in range(1,5):
+#    print(y)
+    
+#for y in range(1,10,2):
+#    print(y)
+#for y in range(10,0,-1):
+#    print(y)
+
+#for y in range(10,0,1):
+#    print(y)
+
+#inputtext = "John28"
+#for character in inputtext:
+#    print(character)
+
+#sentence = input("Enter a sentence: ")
+#spaceCount = 0
+#for character in sentence:
+#    if character == " ":
+#        spaceCount+=1
+#wordCount = spaceCount+1
+#print("Spaces: ", spaceCount)
+#print("Words: ", wordCount)
+
+#sentence = input("Enter a sentence: ")
+#spaceCount = 0
+#counter = 0
+#while counter <len(sentence):
+#    if sentence[counter]==" ":
+#        spaceCount+=1
+#    counter+=1
+#wordCount = spaceCount+1
+#print("Spaces: ",spaceCount)
+#print("Words: ",wordCount)
+
