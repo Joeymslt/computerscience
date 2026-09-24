d4fb4fdc642ba74829d4bc36359b5ac132563ab7
Joeymslt <22.joey.rohan@msltcastlegregory.ie>
2026-09-24 12:33:02 +0100

    Fix myStringList initialization and add print statements
    Corrected the initialization of myStringList and added print statements for specific elements. Included a check for 'a' in a list and added user input for car names.

--- Chapter 1/hw2.py
+++ Chapter 1/hw2.py
@@ -6,4 +6,23 @@ print(myStringList)
 print(type(myString))
 print(type(myStringList))
 
-myStringList = ['F','r','a','n','k',' '
+myStringList = ['F','r','a','n','k',' ','L','a','m','p','e','y']
+
+print(myStringList[1])
+print(myStringList[3])
+print(myStringList[6])
+
+print(myStringList[2:8])
+
+if 'a' in ['t', 'e', 'a', 'c', 'u', 'p']:
+  print("It is in the list.")
+myCars = ['Fiat', 'Honda', 'Toyota', 'BMW']
+print("Enter a make of car: ")
+carName = input()
+if carName not in myCars:
+print("We do not stock", carName)
+else:
+print("Yes, we stock," carName)
+
+myList = [1, 19, 27, 8, 5, 9]
+print(myList)
